c338c5f1a7783976ac3ce3e5e316f5888919b606
imran5391 <imrankhan5391@gmail.com>
2022-11-30 21:40:45 +0530

    19. Adding the ProducersController

---
+++ eTickets/Controllers/ProducersController.cs
@@ -0,0 +1,25 @@
+﻿using eTickets.Data;
+using eTickets.Data.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eTickets.Controllers
+{
+    public class ProducersController : Controller
+    {
+        private readonly IProducersService _service;
+        public ProducersController(IProducersService service)
+        {
+            _service = service;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var allProducers = await _service.GetAllAsync();
+            return View(allProducers);
+        }
+    }
+}
