eb09c2e07bf847d25a721d7c84ec437d660e3abc
imran5391 <imrankhan5391@gmail.com>
2022-12-01 09:10:20 +0530

    44.Adding new producer to database, check validation

--- eTickets/Models/Producer.cs
+++ eTickets/Models/Producer.cs
@@ -13,10 +13,15 @@ namespace eTickets.Models
         public int Id { get; set; }
 
         [Display(Name = "Profile Picture")]
+        [Required(ErrorMessage = "Profile Picture is required")]  //validation
         public string ProfilePictureURL { get; set; }
+
         [Display(Name = "FullName")]
+        [Required(ErrorMessage = "Full Name is required")]
+        [StringLength(50, MinimumLength = 3, ErrorMessage = "Full name must be between 3 and 50 chars")]
         public string FullName { get; set; }
         [Display(Name = "Biography")]
+        [Required(ErrorMessage = "Biography is required")]
         public string Bio { get; set; }
 
         //Relationships
