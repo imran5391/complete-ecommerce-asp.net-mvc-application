13eade47571132beae5d976e1213372ca6d93911
imran5391 <imrankhan5391@gmail.com>
2022-12-01 07:07:38 +0530

    37. Adding EntityBaseRepository files |

--- eTickets/Data/Base/IEntityBaseRepository.cs
+++ eTickets/Data/Base/IEntityBaseRepository.cs
@@ -5,6 +5,10 @@ using System.Threading.Tasks;
 
 namespace eTickets.Data.Base
 {
+    //In the ASP.NET MVC application you have completed the Actor's section by adding the Create, Read, Update and Delete (CRUD) functionalities.
+    //Now, you will need to create the same methods for the Cinemas, Producers, and even Movies. But, instead of repeating the same code over
+    //and over again, we are to create a generic base repository.Base or generic repositories are helpful for the main functionality of the app.
+    //You code less and you achieve the same goal. On this part, you will add all the necessary EntityBaseRepository files. 
     public interface IEntityBaseRepository<T> where T:class, IEntityBase,new()
     {
         Task<IEnumerable<T>> GetAllAsync();
