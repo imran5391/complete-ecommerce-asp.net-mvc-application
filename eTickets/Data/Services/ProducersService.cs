496f7d5a7ad402b4733023fcadd11e6d0a056cac
imran5391 <imrankhan5391@gmail.com>
2022-12-01 07:59:39 +0530

    42. ProducersService configuration with EntityBaseRepository, Adding ProducersService is inherit from the EnityBaseRepository also inherit from Producer interface, complete the whool flow lets add service configuratio in startup.cs fiel

---
+++ eTickets/Data/Services/ProducersService.cs
@@ -0,0 +1,18 @@
+﻿using eTickets.Data.Base;
+using eTickets.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eTickets.Data.Services
+{
+    //Adding ProducersService is inherit from the EnityBaseRepository also inherit from Producer interface
+    public class ProducersService: EntityBaseRepository<Producer>, IProducersService
+    {
+        public ProducersService(AppDbContext context):base(context) 
+        {
+
+        }
+    }
+}
